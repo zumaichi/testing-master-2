e79d7438f0dd4b2e597b6d93aebd2092168be4ed
zumaichi <dzumaquero@gmail.com>
2026-02-22 12:47:58 +0100

    opcional

---
+++ cypress/e2e/login.spec.ts
@@ -0,0 +1,58 @@
+describe('login specs', () => {
+  it('visita pagina de login', () => {
+    // Arrange
+
+    // Act
+    cy.visit('/');
+
+    // Assert
+  });
+
+  it('al hacer click en el input hace focus', () => {
+    // Arrange
+
+    // Act
+    cy.visit('/');
+    cy.findByRole('textbox', { name: /usuario/i }).click();
+
+    // Assert
+    cy.findByRole('textbox', { name: /usuario/i }).should('have.focus');
+  });
+
+  it('login correcto', () => {
+    // Arrange
+    const user = 'admin';
+    const password = 'test';
+
+    // Act
+    cy.visit('/');
+    cy.get('input[name=user]').as('userInput');
+    cy.get('input[name=password]').as('passwordInput');
+
+    cy.get('@userInput').type(user);
+    cy.get('@passwordInput').type(password);
+    cy.get('button[type=submit]').click();
+
+    // Assert
+    cy.url().should('include', '/submodule-list');
+  });
+
+  it('login incorrecto', () => {
+    // Arrange
+    const user = '1234';
+    const password = '1234';
+
+    // Act
+    cy.visit('/');
+    cy.get('input[name=user]').as('userInput');
+    cy.get('input[name=password]').as('passwordInput');
+
+    cy.get('@userInput').type(user);
+    cy.get('@passwordInput').type(password);
+    cy.get('button[type=submit]').click();
+
+    // Assert
+    cy.get('@userInput').should('have.value', user);
+    cy.get('@passwordInput').should('have.value', password);
+  });
+});
