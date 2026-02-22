aa658a97091bff9dd7c3c8533a573a56a7e1c4e4
zumaichi <dzumaquero@gmail.com>
2026-02-22 12:07:28 +0100

    antet de pr

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   allowCypressEnv: false,
 
   e2e: {
-      baseUrl: 'http://localhost:4000/#',
-      specPattern: 'cypress/e2e/**/*.spec.{js,jsx,ts,tsx}',
-    },
+    baseUrl: 'http://localhost:80800',
+    specPattern: 'cypress/e2e/**/*.spec.{js,jsx,ts,tsx}',
   },
-);
+});
