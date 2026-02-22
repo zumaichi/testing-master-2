64688d2a398b1db70bbbc2841582e4d628ef35eb
zumaichi <dzumaquero@gmail.com>
2026-02-22 12:10:15 +0100

    0.1

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   allowCypressEnv: false,
 
   e2e: {
-    baseUrl: 'http://localhost:80800',
+    baseUrl: 'http://localhost:8080',
     specPattern: 'cypress/e2e/**/*.spec.{js,jsx,ts,tsx}',
   },
 });
